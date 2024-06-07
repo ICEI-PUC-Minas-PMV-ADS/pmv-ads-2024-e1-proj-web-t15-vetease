5804a1267434fe79ade2017c925253ec9b7164f1
ZXbatata123 <augustuscesar891@gmail.com>
2024-06-07 00:53:25 -0300

    update em controle de agendamentos e cadastro de prescrição

--- src/scripts/services-render.js
+++ src/scripts/services-render.js
@@ -15,6 +15,24 @@ document.addEventListener('DOMContentLoaded', () => {
               const price = clone.querySelector('.service-detail h5');
               const button = clone.querySelector('.btn-submit');
 
+              if (clone.querySelector('.service-detail h6') != undefined) {
+                const status = clone.querySelector('.service-detail h6') 
+                if (service.status == "cancelled") {
+                    status.className = "cancelled"
+                    status.innerText = "Cancelado"
+                }
+                if (service.status == "waiting") {
+                    status.className = "waiting"
+                    status.innerText = "Aguardando"
+                }
+                if (service.status == "prescription") {
+                    const button = clone.querySelector('.service-detail button') 
+                    button.type = "button"
+                    button.innerText = "Prescrição"
+                    button.className = "btn-submit"
+                }
+              }
+
               img.src = service.imgSrc;
               title.textContent = service.title;
               description.textContent = service.description;
