22f3f826d220f0ca5ec73d05c13cfb58ecf1fcd5
Thiago Murtinho <thiago.murtinho@live.com>
2024-06-23 18:35:26 -0300

    refactor: Update modal-prescricao.mjs to pre-fill prescription fields

--- src/scripts/modal-prescricao.mjs
+++ src/scripts/modal-prescricao.mjs
@@ -1,17 +1,23 @@
 import { lerDoLocalStorage, lerCookie } from "./utils.mjs";
 
-function showPrescricaoDialog() {
+function preencherCampos(tutor_email, pet_name, service) {
+    const schedules = lerDoLocalStorage('schedules-db') || [];
+    const schedule = schedules.find(schedule => `${schedule.tutor_email}${schedule.pet_name}${schedule.service}` === `${tutor_email}${pet_name}${service}`);
+  
+    document.getElementById('prescricao-dialog').value = schedule.prescricao || '';
+  }
+
+function showPrescricaoDialog(tutor_email, pet_name, service) {
     const dialog = document.getElementById('prescricao-dialog');
     const schedules = lerDoLocalStorage('schedules-db') || [];
-    const email = lerCookie('user::email');
+    const schedule = schedules.find(schedule => `${schedule.tutor_email}${schedule.pet_name}${schedule.service}` === `${tutor_email}${pet_name}${service}`);
+    preencherCampos(tutor_email, pet_name, service)
 
-    const schedule = schedules.find(schedule => schedule.tutor_email === email )
-    console.log("🚀 ~ file: modal-prescricao.mjs:9 ~ showPrescricaoDialog ~ schedule:", schedule)
     const dialogTitle = document.getElementById('dialog-prescricao-title');
     const dialogDescription = document.getElementById('dialog-prescricao');
 
     dialogTitle.textContent = `Prescrição - ${schedule?.tutor_name ?? "Tutor"}  - ${schedule?.pet_name} Pet`;
-    dialogDescription.textContent = schedule.prescricao;
+    dialogDescription.textContent = schedule.prescricao ?? 'Nenhuma prescrição cadastrada';
 
 
     dialog.showModal();
